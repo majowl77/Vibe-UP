ccd1c5a4701ad7a10236ecce036187d07f80638c
Majedah <122641068+majowl77@users.noreply.github.com>
2024-02-04 09:04:18 +0300

    SideBar

--- src/sideBar/SideBar.tsx
+++ src/sideBar/SideBar.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { Flex, Text, Button } from "@radix-ui/themes";
+import { Flex, Text, Button, Box } from "@radix-ui/themes";
 
 export default function SideBar() {
   return (
-    <div>
-      <h1> MenuTabs </h1>
+    <section className="sideBar">
+      <h3> MenuTabs </h3>
       <div>
-        <Button>Bookmark</Button>
-        <Button variant="ghost">Edit profile</Button>
+        <Flex gap="3" direction="column" align="start" justify="start">
+          <Box width="9" height="9">
+            <Button variant="ghost">Home</Button>
+          </Box>
+          <Box width="9" height="9">
+            <Button variant="ghost">Explore</Button>
+          </Box>
+          <Box width="9" height="9">
+            <Button variant="ghost">People</Button>
+          </Box>
+          <Box width="9" height="9">
+            <Button variant="ghost">Saved</Button>
+          </Box>
+          <Box width="9" height="9">
+            <Button variant="ghost">Create Post</Button>
+          </Box>
+        </Flex>
       </div>
-    </div>
+    </section>
   );
 }
